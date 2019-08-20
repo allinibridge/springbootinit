e884719c4359100de0b32a1d46876a1d48dab77a
zpf0408 <632998530@qq.com>
2019-08-20 18:27:42 +0800

    数据库连接池换成druid

--- src/test/java/com/mrspeak/springbootinit/SpringbootinitApplicationTests.java
+++ src/test/java/com/mrspeak/springbootinit/SpringbootinitApplicationTests.java
@@ -4,16 +4,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sql.DataSource;
 import java.math.BigDecimal;
+import java.sql.Connection;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootinitApplicationTests {
 
     Logger logger = LoggerFactory.getLogger(SpringbootinitApplication.class);
+
+    @Autowired
+    DataSource dataSource;
+
     @Test
     public void contextLoads() {
         System.out.println(0.06+0.01);
@@ -24,6 +31,16 @@ public class SpringbootinitApplicationTests {
         logger.info(String.valueOf(0.06+0.01));
         logger.warn("你好呀！！");
 
+    }
+
+    @Test
+    public void jdbcTest(){
+        System.out.println(dataSource.getClass());
+        try (Connection connection = dataSource.getConnection()) {
+            System.out.println(connection);
+        }catch (Exception e){
+            logger.info(e.getMessage());
+        }
     }
 
 }
